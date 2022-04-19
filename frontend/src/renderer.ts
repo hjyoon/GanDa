40ab089f9b7ff5606044e1ec5d115b8b673aab97
hjyoon <se1620236@naver.com>
2022-04-19 23:52:02 +0900

    Add prettier and eslint. Closed #S06P31S106-40.

--- frontend/src/renderer.ts
+++ frontend/src/renderer.ts
@@ -26,9 +26,9 @@
  * ```
  */
 
-import "./main.css";
-import "./index";
+import './main.css';
+import './index';
 
-console.log(
-  '👋 This message is being logged by "renderer.js", included via webpack'
-);
+// console.log(
+// 	'👋 This message is being logged by "renderer.js", included via webpack'
+// );
