05a008811b9975929b1f11961a4d199270b82eb4
hjyoon <se1620236@naver.com>
2022-04-23 19:40:05 +0900

    Implement router and mock pages. Closed #S06P31S106-58.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import FirstPage from './pages/FirstPage';
+import SecondPage from './pages/SecondPage';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
 	<React.StrictMode>
-		<App />
+		<HashRouter>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route index element={<FirstPage />} />
+					<Route path="first" element={<FirstPage />} />
+					<Route path="second" element={<SecondPage />} />
+				</Route>
+			</Routes>
+		</HashRouter>
 	</React.StrictMode>
 );
---
+++ frontend/src/pages/FirstPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+
+function FirstPage() {
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+				height: '100vh',
+			}}
+		>
+			<Typography variant="h5">이곳은 첫 번째 페이지 입니다.</Typography>
+		</Box>
+	);
+}
+
+export default FirstPage;
