3fe1ed054b5f16960f4b66335ea1f50a35573a72
hjyoon <se1620236@naver.com>
2022-04-26 16:49:46 +0900

    Add Material-UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { CssBaseline, Typography, Container, Box, Button } from '@mui/material';
+
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<>
+			<CssBaseline />
+			<Container>
+				<Box
+					sx={{
+						display: 'flex',
+						height: '100vh',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Typography variant='h4'>Hello World</Typography>
+				</Box>
+			</Container>
+		</>
+	);
 }
 
 export default App;
