6329dcfce04991856db25cecd4475ce7dbb90c47
ruokic <gusqo327@naver.com>
2022-05-06 13:43:52 +0900

    feat #S06P31S106-68 : change Router.tsx

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Upload from './pages/Upload';
+import Generation from './pages/Generation';
+import Main from './pages/Main';
 import Index from './pages/Index';
 import Tuning from './pages/Tuning';
 
@@ -8,7 +9,8 @@ function Router() {
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<Index />}>
-					<Route index element={<Upload />} />
+					<Route index element={<Main />} />
+					<Route path='generate' element={<Generation />} />
 					<Route path='tuning' element={<Tuning />} />
 				</Route>
 			</Routes>
