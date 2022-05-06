b6006a1264b538a4d7c61f6dbef4497f676fc034
hjyoon <se1620236@naver.com>
2022-05-06 11:26:17 +0900

    Add dev and prod mode. Closed #S06P31S106-89.

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000';
+const baseURL = `${process.env.REACT_APP_PROTOCOL}://${window.location.hostname}:${process.env.REACT_APP_PORT}`;
 const api = axios.create({
 	baseURL,
 });
