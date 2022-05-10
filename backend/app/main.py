9f186cc81811279cd6ac8bd2fd36f8c0b4cffd06
권도형 <dohyung102@naver.com>
2022-05-10 13:18:15 +0900

    feat #S06P31S106-110 : add logic

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,32 @@
+from io import BytesIO
+import json
 from fastapi import FastAPI, UploadFile, File, BackgroundTasks
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import JSONResponse, FileResponse, Response
+from fastapi.middleware.cors import CORSMiddleware
 
 import os
 from os import getcwd
 from PIL import Image
-from typing import List
+from typing import List, Optional
 import datetime
 import requests
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 PATH_FILES = getcwd() + "/"
 BASE_URL = "http://k6s106.p.ssafy.io:8010/"
-image_generation = "api/gen-image/"
+# BASE_URL = "http://127.0.0.1:8010/"
+image_url = "api/gen-image/"
+model_url = "api/data-list/"
+pkl_url = "api/pkl/"
 
 
 def resize_image(filename: str):
@@ -33,19 +46,8 @@ def resize_image(filename: str):
     print("success")
 
 
-@app.post("/upload/file2/")
-async def upload_file(background_tasks: BackgroundTasks):
-    url = f"{BASE_URL}{image_generation}"
-    
-    if not os.path.exists("static"):
-        os.mkdir("static")
-    
-    # sample output
-    data = requests.get(url)
-    
-    with open("static/test.png", "wb") as f:
-        f.write(data.content)
-    
+@app.post("/api/gen-image/{data_id}/")
+async def upload_file(background_tasks: BackgroundTasks, data_id: str, count: Optional[int] = 1):
     '''    
     file_urls = []
     for file in in_files:
@@ -59,7 +61,142 @@ async def upload_file(background_tasks: BackgroundTasks):
         file_urls.append(PATH_FILES + file.filename)
     '''
     
-    return FileResponse("static/test.png")
-    # return JSONResponse(data.json())
-    # return FileResponse(file_urls[0])
-    # return JSONResponse(content={"message": file_urls})
+    if not os.path.exists("static/images"):
+        os.mkdir("static/images")
+    
+    params = {
+        'count' : count
+    }
+
+    url = f"{BASE_URL}{image_url}{data_id}"
+    data = requests.get(url, params=params)
+    if data.status_code == 200:
+        with open(f"static/images/{data_id}.png", "wb") as f:
+            f.write(data.content)
+        
+        return FileResponse(f"static/images/{data_id}.png")
+    return Response(status_code=data.status_code)
+
+
+@app.get("/api/data-list/")
+async def read_data_list():
+    url = f"{BASE_URL}{model_url}"
+    data = requests.get(url)
+    if data.status_code == 200:
+        data_str = data.content.decode('utf8').replace("'", '"')
+        data_json = json.loads(data_str)
+        return data_json
+    return Response(status_code=data.status_code)
+
+
+@app.post("/api/data-list/")
+async def create_data_list(
+    pkl_file: UploadFile, 
+    name: Optional[str] = None, 
+    img: Optional[UploadFile] = None,
+    description: Optional[str] = None,
+):
+    if not os.path.exists("static/images"):
+        os.mkdir("static/images")
+    if not os.path.exists("static/pkls"):
+            os.mkdir("static/pkls")
+
+    img_content = await img.read()
+    pkl_content = await pkl_file.read()
+    with open(f"static/images/{img.filename}", "wb") as f:
+        f.write(img_content)
+    with open(f"static/pkls/{pkl_file.filename}", "wb") as f:
+        f.write(pkl_content)
+
+    files = {
+        'img' : (f"{img.filename}", open(f"static/images/{img.filename}",'rb'), f"{img.content_type}"),
+        'pkl_file' : (f"{pkl_file.filename}", open(f"static/pkls/{pkl_file.filename}",'rb'), f"{pkl_file.content_type}"),
+    }
+    params = {
+        'name' : name,
+        'description' : description
+    }
+
+    url = f"{BASE_URL}{model_url}"
+    data = requests.post(url, params=params, files=files)
+    if data.status_code == 200:
+        data_str = data.content.decode('utf8').replace("'", '"')
+        data_json = json.loads(data_str)
+        return data_json
+    return Response(status_code=data.status_code)
+
+
+@app.patch("/api/data-list/{data_id}/")
+async def update_data(
+    data_id: int,
+    name: Optional[str] = None, 
+    img: Optional[UploadFile] = None,
+    description: Optional[str] = None,
+):
+    if not os.path.exists("static/images"):
+        os.mkdir("static/images")
+
+    img_content = await img.read()
+    with open(f"static/images/{img.filename}", "wb") as f:
+        f.write(img_content)
+
+    files = {
+        'img' : (f"{img.filename}", open(f"static/images/{img.filename}",'rb'), f"{img.content_type}")
+    }
+    params = {
+        'name' : name,
+        'description' : description
+    }
+
+    url = f"{BASE_URL}{model_url}{data_id}/"
+    data = requests.patch(url, params=params, files=files)
+    if data.status_code == 200:
+        data_str = data.content.decode('utf8').replace("'", '"')
+        data_json = json.loads(data_str)
+        return data_json
+    return Response(status_code=data.status_code)
+
+
+@app.delete("/api/data-list/{data_id}/")
+async def delete_data(
+    data_id: int,
+):
+
+    url = f"{BASE_URL}{model_url}{data_id}/"
+    data = requests.delete(url)
+    if data.status_code == 200:
+        data_str = data.content.decode('utf8').replace("'", '"')
+        data_json = json.loads(data_str)
+        return data_json
+    return Response(status_code=data.status_code)
+
+
+@app.patch("/api/pkl/rename/{data_id}/")
+async def pkl_rename(
+    data_id: str,
+    new_name: Optional[str] = None, 
+):
+
+    params = {
+        'new_name' : new_name,
+    }
+
+    url = f"{BASE_URL}{pkl_url}rename/{data_id}/"
+    data = requests.patch(url, params=params)
+    if data.status_code == 200:
+        return new_name
+    return Response(status_code=data.status_code)
+
+
+@app.get("/api/pkl/download/{data_id}/")
+def pkl_download(
+    data_id: str,
+):
+
+    url = f"{BASE_URL}{pkl_url}download/{data_id}/"
+    data = requests.get(url)
+    if data.status_code == 200:
+        with open(f"static/pkls/{data_id}.pkl", "wb") as f:
+            f.write(data.content)
+        return FileResponse(f"static/pkls/{data_id}.pkl")
+    return Response(status_code=data.status_code)
