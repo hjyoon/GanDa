dd018eb34320963eb6954f9ddc82ab02b73aa3ab
이영준 <jun2korea@naver.com>
2022-05-20 09:45:15 +0900

    docs : readme update

--- ai/README.md
+++ ai/README.md
@@ -1,8 +1,8 @@
 # AI README
 
-# 훈련
+# 이미지 생성
 
-# 이미지 생성을 위한 서버
+## 이미지 생성을 위한 서버
 - app 디렉터리의 코드 단독으로 동작하지 않습니다.
 - 다음의 두가지 방법 중 한가지를 권장합니다.
   - StyleGAN3기반으로 설정되 git clone
@@ -13,19 +13,20 @@
 ## StyleGAN3에 설정된 git clone
 - [StyleGAN3 + FastAPI](https://github.com/mintropy/stylegan3) clone
 - conda 가상환경 설정
+- `main.bash` 파일을 활용하여 실행
+
+```bash
+$ bash main.bash
+```
 
 ## StyleGAN3을 받아서 직접 설정
 - [StyleGAN3](https://github.com/NVlabs/stylegan3) clone
 - app 디렉터리의 모든 파일 이동
 - conda 가상환경 설정
+- `main.bash`를 통하여 실행하는 경우 conda 가상환경 이름이 달라서 문제 발생할 수 있음
+    - `main.bash` 5번줄을 `conda activate stylegan3` 로 변경 후 실행할 수 있음
 
-## conda 가상환경 설정 후
-- 다음 순서를 거쳐 실행
-    - conda 가상환경 실행
-    - pip install
-    - 필요한 디렉터리 생성
-    - 서버 실행
-
+## conda 가상환경 설정 후 수동으로 실행
 ```bash
 # conda 가상환경 생성
 $ conda env create -f enviroment.yml
@@ -39,9 +40,7 @@ $ python setup.py
 $ uvicorn main:app --host 0.0.0.0 --port 8010
 ```
 
-
-
 # 학습하는 방법
-
 * 학습에 gpu서버를 필요로 하므로 colab에서 실행이 가능하도록 ipynb파일로 정리했습니다.
 * 해당 파일을 활용하여 colab에서 실행해보시길 바랍니다.
+- `start_train.py` 파일을 활용하는 경우, StyleGAN3 github을 클론 받아 사용하는 것을 권장합니다.
--- ai/app/main.py
+++ ai/app/main.py
@@ -1,11 +1,13 @@
 import datetime
 import shutil
+import io
 import json
-from typing import Optional
+from typing import Optional, List
 import os
+import zipfile
 
 
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Query
 from fastapi.responses import FileResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
@@ -18,6 +20,9 @@ import dnnlib
 import legacy
 
 
+# set maximum image generate
+MAXIMUM_IMAGE_GENERATE = 2
+
 app = FastAPI()
 
 app.add_middleware(
@@ -27,37 +32,39 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/api/images", StaticFiles(directory="data/images"), name="images")
+app.mount(
+    "/api/images", 
+    StaticFiles(directory="data/images/"), 
+    name="images"
+)
 
 
-@app.get("/api/gen-image/{data_id}/")
-def gen_images(data_id: str, count: Optional[int] = 1):
-    network_pkl = "https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-afhqv2-512x512.pkl"
-    imgs = []
+@app.get(
+    "/api/gen-image/{data_id}/",
+    tags=["images"],
+)
+async def gen_images(data_id: str, count: Optional[int] = 1):
 
     with open("data/data.json", "r") as f:
         data = json.load(f)
     if data_id != "0" and data_id not in data:
         return Response(status_code=404)
 
+    if count > MAXIMUM_IMAGE_GENERATE:
+        count = MAXIMUM_IMAGE_GENERATE
     # input parameter
     is_cuda = torch.cuda.is_available()
     device = torch.device('cuda' if is_cuda else 'cpu')
-    seeds = np.random.randint(2**32 - 1, size=1)
     latent_vector = None
     truncation_psi = 0.95
     noise_mode = 'random'
 
     # parameter rename
-    seeds_ = seeds
     latent_vector = None
     truncation_psi_ = truncation_psi
     noise_mode_ = noise_mode
     # network = network_pkl
-    if data_id == "0":
-        network = network_pkl
-    else:
-        network = f"data/{data[data_id]['pkl']}"
+    network = f"data/{data[data_id]['pkl']}"
 
     with dnnlib.util.open_url(network) as f:
         G = legacy.load_network_pkl(f)['G_ema'].to(device)
@@ -67,36 +74,76 @@ def gen_images(data_id: str, count: Optional[int] = 1):
     #     label[:, class_idx] = 1
     class_idx = label
 
-    if latent_vector == None : 
-        for seed_idx, seed in enumerate(seeds_):
-            z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
-            img = G(z, class_idx, truncation_psi=truncation_psi_, noise_mode=noise_mode_)
-            img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-            imgs.append(PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB'))
-    else:
-        for i in range(len(latent_vector)):
-            img = G(latent_vector[i], class_idx, truncation_psi=truncation_psi_, noise_mode=noise_mode_)
-            img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-            imgs.append(PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB'))
-    img = imgs[0]
-    img.save(f'data/test.png')
+    if os.path.isdir("data/images/gen"):
+        shutil.rmtree("data/images/gen")
+    os.mkdir("data/images/gen")
+    imgs = []
+    imgs_path = []
+    for i in range(count):
+        seeds = np.random.randint(2**32 - 1, size=1)
+        seeds_ = seeds
+        if latent_vector == None :
+            for seed_idx, seed in enumerate(seeds_):
+                z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+                img = G(z, class_idx, truncation_psi=truncation_psi_, noise_mode=noise_mode_)
+                img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+                imgs.append(PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB'))
+        else:
+            for i in range(len(latent_vector)):
+                img = G(latent_vector[i], class_idx, truncation_psi=truncation_psi_, noise_mode=noise_mode_)
+                img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+                imgs.append(PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB'))
+        img = imgs[-1]
+        img.save(f'data/images/gen/{data_id}_{i}.png')
+        imgs_path.append(f'data/images/gen/{data_id}_{i}.png')
     
-    return FileResponse("data/test.png")
+    if count == 1:
+        return FileResponse(f"data/images/gen/{data_id}_0.png")
+    zip_filename = "archive.zip"
+    s = io.BytesIO()
+    zf = zipfile.ZipFile(s, "w")
+
+    for fpath in imgs_path:
+        # Calculate path for file in zip
+        fdir, fname = os.path.split(fpath)
+        # Add file, at correct path
+        zf.write(fpath, fname)
+    
+    # Must close zip for all contents to be written
+    zf.close()
+    # Grab ZIP file from in-memory, make response with correct MIME-type
+    resp = Response(
+        s.getvalue(), 
+        media_type="application/x-zip-compressed", 
+        headers=
+            {
+            'Content-Disposition': f'attachment;filename={zip_filename}'
+            }
+    )
+    return resp
 
 
-@app.get("/api/data-list/")
-def read_data_list():
+@app.get(
+    "/api/data-list/",
+    tags=["data list"],
+)
+async def read_data_list():
     with open("data/data.json") as f:
         data = json.load(f)
     return data
 
 
-@app.post("/api/data-list/")
-def create_data_list(
+@app.post(
+    "/api/data-list/",
+    tags=["data list"],
+)
+async def create_data_list(
     pkl_file: UploadFile, 
     name: Optional[str] = None, 
-    img: Optional[UploadFile] = None,
+    img: Optional[str] = None,
     description: Optional[str] = None,
+    fid: Optional[float] = Query(default=None, gt=0),
+    kimg: Optional[int] = Query(default=None, gt=0),
 ):
     if (
         pkl_file.content_type != "application/octet-stream"
@@ -104,14 +151,9 @@ def create_data_list(
             len(pkl_file.filename) >= 4
             and pkl_file.filename[-4:] != '.pkl'
         )
-        or (
-            img is not None
-            and img.content_type[:5] != "image"
-        )
     ):
         return Response(status_code=400)
     
-    # def file & imgage path
     pkl_file_name = pkl_file.filename
     if name is None:
         name = pkl_file_name
@@ -122,78 +164,77 @@ def create_data_list(
         pkl_file_name = f"{pkl_file_name[:-4]}-{t}.pkl"
     with open(f"data/{pkl_file_name}", "wb") as f:
         shutil.copyfileobj(pkl_file.file, f)
-    if img is not None:
-        img_name = img.filename
-        if os.path.isfile(f"data/images/{img_name}"):
-            dot_idx = img_name.rindex(".")
-            img_name = f"{img_name[:dot_idx]}-{t}{img_name[dot_idx:]}"
-        with open(f"data/images/{img_name}", "wb") as image:
-            shutil.copyfileobj(img.file, image)
-    else:
-        img_name = None
     num = 1 if not data else (int(max(data.keys())) + 1)
     data[num] = {
         "pkl": pkl_file_name,
         "name": name,
-        "image": img_name,
-        "description": description
+        "image": img,
+        "description": description,
+        "fid": fid,
+        "kimg": kimg,
     }
     with open("data/data.json", "w") as f:
         json.dump(data, f, indent=2)
     return data
 
 
-@app.patch("/api/data-list/{data_id}/")
-def update_data(
+@app.patch(
+    "/api/data-list/{data_id}/",
+    tags=["data list"],
+)
+async def update_data(
     data_id: str,
     name: Optional[str] = None, 
-    img: Optional[UploadFile] = None,
+    img: Optional[str] = None,
     description: Optional[str] = None,
+    fid: Optional[float] = Query(default=None, gt=0),
+    kimg: Optional[int] = Query(default=None, gt=0),
 ):
     with open("data/data.json", "r") as f:
         data = json.load(f)
+    data_id = str(data_id)
     if data_id not in data:
         return Response(status_code=404)
-    t = datetime.datetime.now().isoformat(timespec="seconds").replace(":", "")
     if name is not None:
         data[data_id]["name"] = name
     if img is not None:
-        prev_img = data[data_id]["image"]
-        os.remove(f"data/images/{prev_img}")
-        img_name = img.filename
-        if os.path.isfile(f"data/images/{img_name}"):
-            dot_idx = img_name.rindex(".")
-            img_name = f"{img_name[:dot_idx]}-{t}{img_name[dot_idx:]}"
-        with open(f"data/images/{img_name}", "wb") as image:
-            shutil.copyfileobj(img.file, image)
-        data[data_id]["image"] = img_name
+        data[data_id]["image"] = img
     if description is not None:
         data[data_id]["description"] = description
+    if fid is not None:
+        data[data_id]["fid"] = fid
+    if kimg is not None:
+        data[data_id]["kimg"] = kimg
     with open("data/data.json", "w") as f:
         json.dump(data, f, indent=2)
     return data
 
 
-@app.delete("/api/data-list/{data_id}/")
-def update_data(
-    data_id: str,
+@app.delete(
+    "/api/data-list/{data_id}/",
+    tags=["data list"],
+)
+async def delete_data(
+    data_id: int,
 ):
     with open("data/data.json", "r") as f:
         data = json.load(f)
+    data_id = str(data_id)
     if data_id not in data:
         return Response(status_code=404)
-    img, pkl = data[data_id]["image"], data[data_id]["pkl"]
+    pkl = data[data_id]["pkl"]
     os.remove(f"data/{pkl}")
-    if img is not None:
-        os.remove(f"data/images/{img}")
     data.pop(data_id)
     with open("data/data.json", "w") as f:
         json.dump(data, f, indent=2)
     return data
 
 
-@app.patch("/api/pkl/rename/{data_id}/")
-def pkl_rename(
+@app.patch(
+    "/api/pkl/rename/{data_id}/",
+    tags=["pkl"],
+)
+async def pkl_rename(
     data_id: str,
     new_name: str
 ):
@@ -213,8 +254,11 @@ def pkl_rename(
     return new_name
 
 
-@app.get("/api/pkl/download/{data_id}/")
-def pkl_download(
+@app.get(
+    "/api/pkl/download/{data_id}/",
+    tags=["pkl"],
+)
+async def pkl_download(
     data_id: str,
 ):
     with open("data/data.json", "r") as f:
@@ -223,3 +267,58 @@ def pkl_download(
         return Response(status_code=404)
     pkl = data[data_id]["pkl"]
     return FileResponse(f"data/{pkl}")
+
+
+@app.get(
+    "/api/train/image/{data_id}/",
+    tags=["train"],
+)
+async def train_image(
+    data_id: str,
+):
+    file_path = f"./data/images/train/{data_id}"
+    if os.path.isdir(file_path):
+        file_list = os.listdir(file_path)
+    else:
+        file_list = []
+    return file_list
+
+
+@app.post(
+    "/api/train/image/{data_id}/",
+    tags=["train"],
+)
+async def upload_train_image(
+    data_id: str,
+    images: List[UploadFile],
+):
+    with open("data/data.json", "r") as f:
+        data = json.load(f)
+    if data_id not in data:
+        return Response(status_code=404)
+    file_path = f"data/images/train/{data_id}"
+    if not os.path.isdir(file_path):
+        os.mkdir(file_path)
+    for image in images:
+        image_path = f"{file_path}/{image.filename}"
+        if not os.path.isfile(image_path):
+            with open(image_path, "wb") as img:
+                shutil.copyfileobj(image.file, img)
+    return data_id
+
+
+@app.delete(
+    "/api/train/image/{data_id}/",
+    tags=["train"],
+)
+async def upload_train_image(
+    data_id: str,
+):
+    with open("data/data.json", "r") as f:
+        data = json.load(f)
+    if data_id not in data:
+        return Response(status_code=404)
+    file_path = f"data/images/train/{data_id}"
+    if os.path.isdir(file_path):
+        shutil.rmtree(file_path)
+    return Response(status_code=204)
